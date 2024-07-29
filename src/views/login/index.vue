099b45b9b3b7f10a8e87cfa03932d176b2f2e69e
沫雨熙 <moyuxi@moyuxidndiannao.bbrouter>
2024-07-29 11:35:44 +0800

    注册页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,22 +4,76 @@
 			<!-- logo -->
 			<div class="logo">logo</div>
 			
-			
-			<div class="model-box">
+			<!-- 登录模板 -->
+			<div class="model-box" v-if="!isLogin">
 				<!-- 用户名 -->
 				<van-cell-group>
-					<van-field v-model="form.userName" left-icon="friends-o" placeholder="用户名" />
+					<van-field v-model="form.userName" clearable left-icon="friends-o" :placeholder="isCode ? '手机号' : '用户名'" />
+				</van-cell-group>
+				
+				<van-cell-group v-if="isCode">
+					<van-field v-model="form.tel" left-icon="phone-o" placeholder="验证码" />
+				</van-cell-group>
+				
+				<!-- 密码 -->
+				<van-cell-group v-else>
+					<van-field v-model="form.password" :type="showPwd ? 'password' : 'text'" left-icon="bag-o" placeholder="密码" />
+					<span @click="onShowPwd">
+						<van-icon v-if="showPwd" name="eye-o" />
+						<van-icon v-else name="closed-eye" />
+					</span>
+				</van-cell-group>
+			</div>
+			
+			
+			<!-- 注册模板 -->
+			<div class="model-box" v-else>
+				<!-- 用户名 -->
+				<van-cell-group>
+					<van-field v-model="form.userName" clearable left-icon="friends-o" placeholder="用户名" />
+				</van-cell-group>
+				
+				<!-- 手机号 -->
+				<van-cell-group>
+					<van-field v-model="form.userName" clearable left-icon="friends-o" placeholder="手机号" />
+				</van-cell-group>
+				
+				<!-- 验证码 -->
+				<van-cell-group>
+					<van-field v-model="form.tel" left-icon="phone-o" placeholder="验证码" />
 				</van-cell-group>
 				
 				<!-- 密码 -->
 				<van-cell-group>
-					<van-field v-model="form.password" left-icon="bag-o" placeholder="密码" />
+					<van-field v-model="form.password" :type="showPwd ? 'password' : 'text'" left-icon="bag-o" placeholder="密码" />
+					<span @click="onShowPwd">
+						<van-icon v-if="showPwd" name="eye-o" />
+						<van-icon v-else name="closed-eye" />
+					</span>
 				</van-cell-group>
 				
-				
-				<!-- 登录按钮 -->
+				<!-- 确认密码 -->
+				<van-cell-group>
+					<van-field v-model="form.password" :type="showPwd ? 'password' : 'text'" left-icon="bag-o" placeholder="确认密码" />
+					<span @click="onShowPwd">
+						<van-icon v-if="showPwd" name="eye-o" />
+						<van-icon v-else name="closed-eye" />
+					</span>
+				</van-cell-group>
+			</div>
+			
+			
+			
+			<!-- 登录按钮 -->
+			<div class="login">
 				<div class="login-btn" @click="onLogin">
 					<van-button type="info">登录</van-button>
+				</div>
+				
+				<div class="isLogin">
+					<span @click="onCode">{{isCode ? "密码登录" : "验证码登录"}}</span>
+					<span>|</span>
+					<span @click="onRegistered">{{isLogin ? "用户登录" : "用户注册"}}</span>
 				</div>
 			</div>
 		</div>
@@ -32,25 +86,60 @@ import { Toast } from 'vant';
 export default {
 	data(){
 		return{
+			showPwd: true,
+			isLogin: false,
+			isCode: false,
 			form:{
 				userName: '',
+				tel: '',
 				password: ''
 			}
 		}
 	},
 	methods:{
+		//隐密按钮
+		onShowPwd(){
+			this.showPwd = !this.showPwd
+		},
+		
+		//登录注册切换按钮
+		onRegistered(){
+			this.isLogin = !this.isLogin
+		},
+		
+		//验证码登录按钮
+		onCode(){
+			this.isCode = !this.isCode
+		},
+		
+		//登录注册提交按钮
 		onLogin(){
-			let params = {
-				...this.form
+			if(!this.form.userName){
+				Toast('请输入用户名');
+				return
 			}
-			loginApi(params).then((res)=>{
-				if(res.code == 200){
-					this.$router.push('/home')
-					Toast(res.message);
-				}else{
-					Toast(res.message);
+			if(!this.form.password){
+				Toast('请输入密码');
+				return
+			}
+			
+			
+			if(this.isLogin){
+				let params = {
+					...this.form
 				}
-			})
+				loginApi(params).then((res)=>{
+					if(res.code == 200){
+						this.$router.push('/home')
+						Toast(res.message);
+					}else{
+						Toast(res.message);
+					}
+				})
+			}else{
+				//注册
+			}
+			
 		}
 	}
 }
@@ -63,9 +152,9 @@ export default {
 
 		.login-box {
 			position: absolute;
-			top: 50%;
+			top: 10%;
 			left: 50%;
-			transform: translate(-50%, -50%);
+			transform: translateX(-50%);
 			width: 76%;
 			height: 500px;
 
@@ -77,6 +166,7 @@ export default {
 			.model-box{
 				margin-top: 80px;
 				.van-cell-group{
+					position: relative;
 					margin-top: 30px;
 					border: 1px solid #1989fa ;
 					border-radius: 20px;
@@ -89,18 +179,47 @@ export default {
 							color: #1989fa;
 						}
 					}
+					/*输入框内字体颜色*/
+					::v-deep .van-field__control{
+						color: #FFF;
+					}
+					span{
+						position: absolute;
+						top: 50%;
+						right: 8px;
+						transform: translateY(-50%);
+						color:#fff
+					}
 				}
-				.van-cell-group::after{
+				::v-deep .van-cell-group::after{
 					content: none;
 				}
+				.van-cell::after{
+					content: none;
+				}
+			}
+			
+			//登录模块
+			.login{
 				.login-btn{
 					margin-top: 50px;
 					.van-button{
 						width: 100%;
 						border-radius: 20px;
 					}
-					
 				}
+			}
+			
+			//登录注册切换
+			.isLogin{
+				margin-top: 30px;
+				text-align: center;
+				span{
+					padding: 0 6px;
+					color: #fff;
+					font-size: 13px;
+				}
+				
 			}
 		}
 	}
